e72e5fcb661990f720c6b662a1293dc40d5da7ce
Adrian <adino1191@gmail.com>
2020-08-24 15:35:25 +0200

    Add files via upload

---
+++ Wow_loader/Add new server.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Wow_loader
+{
+    partial class Add_new_server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_new_server));
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxRealmList = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.textBoxName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxName.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxName.ForeColor = System.Drawing.Color.White;
+            this.textBoxName.Location = new System.Drawing.Point(12, 32);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // textBoxRealmList
+            // 
+            this.textBoxRealmList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.textBoxRealmList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxRealmList.ForeColor = System.Drawing.Color.White;
+            this.textBoxRealmList.Location = new System.Drawing.Point(12, 71);
+            this.textBoxRealmList.Name = "textBoxRealmList";
+            this.textBoxRealmList.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRealmList.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonAdd.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonAdd.ForeColor = System.Drawing.Color.White;
+            this.buttonAdd.Location = new System.Drawing.Point(12, 178);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPath.ForeColor = System.Drawing.Color.White;
+            this.labelPath.Location = new System.Drawing.Point(12, 133);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(94, 16);
+            this.labelPath.TabIndex = 5;
+            this.labelPath.Text = "Path to wow.exe";
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.buttonFind.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonFind.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonFind.ForeColor = System.Drawing.Color.White;
+            this.buttonFind.Location = new System.Drawing.Point(12, 149);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 6;
+            this.buttonFind.Text = "Browse...";
+            this.buttonFind.UseVisualStyleBackColor = false;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Server Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Realmlist";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Version";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCancel.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonCancel.ForeColor = System.Drawing.Color.White;
+            this.buttonCancel.Location = new System.Drawing.Point(93, 178);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 10;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelVersion.ForeColor = System.Drawing.Color.White;
+            this.labelVersion.Location = new System.Drawing.Point(12, 109);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(23, 16);
+            this.labelVersion.TabIndex = 11;
+            this.labelVersion.Text = "0.0";
+            // 
+            // Add_new_server
+            // 
+            this.AcceptButton = this.buttonAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(201, 211);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxRealmList);
+            this.Controls.Add(this.textBoxName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Add_new_server";
+            this.Text = "Add new server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Add_new_server_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxRealmList;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelVersion;
+    }
+}
---
+++ Wow_loader/Add new server.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wow_loader
+{
+    public partial class Add_new_server : Form
+    {
+
+
+        public Add_new_server()
+        {
+            InitializeComponent();
+
+            if (Form1.edit == true)
+            {
+                buttonAdd.Text = "Edit";
+                textBoxName.Text = Serializacja.GlobalList.ServerList[Form1.x].Name;
+                textBoxRealmList.Text = Serializacja.GlobalList.ServerList[Form1.x].RealmList;
+                labelVersion.Text = Serializacja.GlobalList.ServerList[Form1.x].Version;
+                labelPath.Text = Serializacja.GlobalList.ServerList[Form1.x].Path;
+            }
+            if (Form1.edit == false)
+            {
+                buttonAdd.Text = "Add";
+            }
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            if (Form1.edit == false)
+            {
+                Serializacja.Deserialization();
+                Serializacja.GlobalList.ServerList.Add(new Serializacja.Server(
+                    textBoxName.Text,
+                    textBoxRealmList.Text,
+                    labelVersion.Text,
+                    labelPath.Text));
+                Serializacja.Serialization();
+                
+                f1.dataGridView1.DataSource = Serializacja.GlobalList.ServerList;
+                this.Close();
+
+                
+                
+                
+            }
+            if (Form1.edit == true)
+            {
+                Serializacja.Deserialization();
+                Serializacja.GlobalList.ServerList[Form1.x] = (new Serializacja.Server(
+                    textBoxName.Text,
+                    textBoxRealmList.Text,
+                    labelVersion.Text,
+                    labelPath.Text));
+                Serializacja.Serialization();
+                f1.dataGridView1.DataSource = Serializacja.GlobalList.ServerList;
+                this.Close();
+
+
+                
+            }
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+                        OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+            openfile.FileName = "Wow";
+
+
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                string sourceFile = openfile.FileName;
+                labelPath.Text = sourceFile;
+                var versionInfo = FileVersionInfo.GetVersionInfo(sourceFile);
+                string version = versionInfo.ProductVersion;
+                labelVersion.Text = version;
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            
+
+            if (string.IsNullOrWhiteSpace(textBoxName.Text) && string.IsNullOrWhiteSpace(textBoxRealmList.Text) && string.IsNullOrWhiteSpace(labelVersion.Text))
+            {
+                this.Close();
+            }
+
+            else if (MessageBox.Show("Do you want to close this window", "WoW Loader", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        public void Add_new_server_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            
+        }
+    }
+}
